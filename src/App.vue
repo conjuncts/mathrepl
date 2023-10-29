fc795b07e2c20461ba7e8ff1eb10975797295400
conjuncts <67614673+conjuncts@users.noreply.github.com>
2023-10-29 10:34:06 -0500

    multi cells

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,35 @@
 <script setup lang="ts">
 
 // import { ref } from "vue";
-import MathCell from "./components/MathCell.vue";
+// import { ref, Ref } from "vue";
+import { ref } from "vue";
+import MathGUI from "./components/MathGUI.vue";
 import "//unpkg.com/mathlive?module";
 
 // console.log(e);
 
 (window as any).runNum = 0;
 
+const cells = ref([0]);
 // function runCell() {
 //     runNum.value++;
 // }
 
+function insertBelow(idx: number) {
+    cells.value.splice(idx+1, 0, cells.value[idx]+1);
+}
 </script>
 
 <template>
     <div class="big-holder">
         <h1>MathREPL</h1>
-
-        <MathCell />
+        
+        <!-- <MathGUI /> -->
+        <MathGUI v-for="(cell, index) in cells" :key="cell">
+            <div class="btn-hider">
+                <button @click="insertBelow(index)">Insert Here</button>
+            </div>
+        </MathGUI>
         <!-- <HelloWorld msg="MathREPL" /> -->
         <p class="read-the-docs"><a href="https://github.com/conjuncts/mathrepl">Github</a></p>
     </div>
@@ -46,4 +57,11 @@ import "//unpkg.com/mathlive?module";
 
 }
 
+button {
+    opacity: 0;
+}
+button:hover {
+    opacity: 1;
+    transition: all 0.5s;
+}
 </style>
---
+++ src/components/MathGUI.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+
+// import { ref } from "vue";
+// import { ref, Ref } from "vue";
+// import { ref } from "vue";
+import MathCell from "./MathCell.vue";
+import "//unpkg.com/mathlive?module";
+
+// console.log(e);
+
+// const idx = ref(0);
+
+// function runCell() {
+//     runNum.value++;
+// }
+
+</script>
+
+<template>
+    <MathCell />
+        <!-- <HelloWorld msg="MathREPL" /> -->
+    <slot/>
+
+</template>
+
+<style scoped>
+
+</style>
